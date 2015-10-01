73efa74a91dadb2be571ea430b55f05e001a8d20
JasonVann <jasonvanet@gmail.com>
2015-09-30 19:51:25 -0700

    Added Ex2.18

---
+++ Chapter2/Ex2.18.scm
@@ -0,0 +1,7 @@
+(define (reverse l)
+  (define (reverse-iter lst result)
+  (cond ((null? lst) result)
+        ((null? (cdr l))
+                (cons (car l) result))
+        (else  (reverse-iter (cdr lst) (cons (car lst) result)))))
+  (reverse-iter l ()))
