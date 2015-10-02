27b04bccd63eedb5dcd92bfeb447981785acb3cf
JasonVann <jasonvanet@gmail.com>
2015-10-01 22:55:50 -0700

    Added Ex2.25

---
+++ Chapter2/Ex2.25.scm
@@ -0,0 +1,13 @@
+(define l (list 1 3 (list 5 7) 9))
+
+; 7
+(car (cdr (car (cdr (cdr l)))))
+; or
+(cadadd l)
+
+(define l (list (list 7)))
+(car (car l))
+
+(define z (list 1 (list 2 (list 3 (list 4 (list 5 (list 6 7)))))))
+
+(cadadr (cadadr (cadadr l)))
