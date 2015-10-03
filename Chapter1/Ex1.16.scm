8e4b649e00f0d972b95b6d7e8c000281ac7164d9
JasonVann <jasonvanet@gmail.com>
2015-10-03 00:12:48 -0700

    Added Ex1.16

---
+++ Chapter1/Ex1.16.scm
@@ -0,0 +1,6 @@
+(define (fast-expt b n)
+    (define (fast-expt-iter a b n)
+        (cond ((= n 1) (* a b))
+                ((even? n) (fast-expt-iter a (* b b) (/ n 2)))
+                (else (fast-expt-iter (* a b) b (- n 1)))))
+    (fast-expt-iter 1 b n))
