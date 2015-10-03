1e002ce79d6e7fe9fac40bf1a4824df8d4d841bb
JasonVann <jasonvanet@gmail.com>
2015-10-02 23:37:38 -0700

    Add Ex1.12

---
+++ Chapter1/Ex1.12.scm
@@ -0,0 +1,7 @@
+(define (pascal row col)
+    (cond ((< col 0) 0)
+            ((and (= col 0) (= row 0)) 1)
+            ((and (= col 0) (not (= row 0))) 1)
+            ((= col row) 1)
+            ((> col row) 0)
+            (else (+ (pascal (- row 1) (- col 1)) (pascal (- row 1) col)))))
