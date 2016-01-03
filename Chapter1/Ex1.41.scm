bab35cde19d4aaef75e52fabb7e8bb8df3d4914a
JasonVann <jasonvanet@gmail.com>
2016-01-03 11:49:51 -0800

    Added Ex1.42

--- Chapter1/Ex1.41.scm
+++ Chapter1/Ex1.41.scm
@@ -4,3 +4,22 @@
 (((double (double double)) inc) 5)
 ; 21
 
+; Explanation from Scheme wiki:
+(((double (double double)) inc) 5) 
+  
+(((double (lambda (x) (double (double x)))) inc) 5) 
+  
+; ... similar expansion omitted 
+  
+((double (double (double (double inc)))) 5) 
+  
+((double (double (double (lambda (x) (inc (inc x)))))) 5) 
+  
+; ... similar expansion omitted 
+  
+(inc (inc (inc (inc (inc (inc (inc (inc (inc (inc (inc (inc (inc (inc (inc (inc 5)))))))))))))))) 
+  
+; ... reduction omitted 
+  
+;21 
+
