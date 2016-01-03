c102b3c80298f66e209f2d5d787ba5c269485186
JasonVann <jasonvanet@gmail.com>
2016-01-03 12:20:47 -0800

    Added Ex1.43

---
+++ Chapter1/Ex1.43.scm
@@ -0,0 +1,10 @@
+(define (compose f g)
+  (lambda (x) (f (g x))))
+(define (repeated f n)
+  (cond ((= n 1) (lambda (x) (f x)))
+        ((= n 2) (compose f f))
+        (else (compose f (repeated f (- n 1))))))
+
+((repeated square 2) 5)
+; 625
+
