47f78cc25e6732e43bc87dbf0665c9405b039f34
JasonVann <jasonvanet@gmail.com>
2016-01-05 23:05:08 -0800

    Updated Ex1.12

--- Chapter1/Ex1.12.scm
+++ Chapter1/Ex1.12.scm
@@ -5,3 +5,12 @@
             ((= col row) 1)
             ((> col row) 0)
             (else (+ (pascal (- row 1) (- col 1)) (pascal (- row 1) col)))))
+
+; another version
+(define (pascal row col)
+  (cond ((< col 0) 0)
+        ((< row col) 0)
+        ((and (= row 0) (= col 0)) 1)
+        (else (+ (pascal (- row 1) (- col 1)) (pascal (- row 1) col))))
+        )
+
