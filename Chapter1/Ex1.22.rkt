0e0d691e11451ce103ea111e696dbbaaf3ecc299
JasonVann <jasonvanet@gmail.com>
2016-01-06 23:01:11 -0800

    Added Ex1.22.rkt

---
+++ Chapter1/Ex1.22.rkt
@@ -0,0 +1,36 @@
+(define (timed-prime-test n)
+  (newline)
+  (display n)
+  (start-prime-test n (current-milliseconds)))
+
+(define (start-prime-test n start-time)
+  (if (prime? n)
+      (report-prime (- (current-milliseconds) start-time))
+      #f))
+
+(define (report-prime elapsed-time)
+  (display " *** ")
+  (display elapsed-time))
+
+(define (prime? n)
+  (= n (smallest-divisor n)))
+
+(define (square n) (* n n))
+
+(define (smallest-divisor n)
+  (find-divisor n 2))
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(define (search-for-primes min)
+    (cond ((< min 0) (error "Negative input"))
+          ((prime? min) (timed-prime-test min))
+             (else (search-for-primes (+ min 1)))))
+
+(search-for-primes 1000)
+
