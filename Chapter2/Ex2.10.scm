5013f77816a118e6b0e5e5f32381aa598d57f2db
JasonVann <jasonvanet@gmail.com>
2016-01-10 11:08:15 -0800

    Added Ex2.10 & Ex2.11

---
+++ Chapter2/Ex2.10.scm
@@ -0,0 +1,36 @@
+(define (make-interval a b) (cons a b))
+
+(define (lower-bound interval)
+  (min (car interval) (cdr interval)))
+
+(define (upper-bound interval)
+  (max (car interval) (cdr interval)))
+
+(define (add-interval x y)
+  (make-interval (+ (lower-bound x) (lower-bound y))
+                 (+ (upper-bound x) (upper-bound y))))
+
+(define (mul-interval x y)
+  (let ((p1 (* (lower-bound x) (lower-bound y)))
+        (p2 (* (lower-bound x) (upper-bound y)))
+        (p3 (* (upper-bound x) (lower-bound y)))
+        (p4 (* (upper-bound x) (upper-bound y))))
+    (make-interval (min p1 p2 p3 p4)
+                   (max p1 p2 p3 p4))))
+
+(define (div-interval x y)
+  (if (and (>= (upper-bound y) 0
+            ) (<= (lower-bound y) 0)
+        ) (error "Spans over zero!")
+  (mul-interval x
+                (make-interval (/ 1.0 (upper-bound y))
+                               (/ 1.0 (lower-bound y))))))
+
+(define (sub-interval x y)
+  (make-interval (- (lower-bound x) (upper-bound y))
+                 (- (upper-bound x) (lower-bound y))))
+
+(define (width inv)
+    (/ (- (upper-bound inv) (lower-bound inv)) 2))
+
+
