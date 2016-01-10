3989a870f19ddc0b8e944765153fd042a05b55fc
JasonVann <jasonvanet@gmail.com>
2016-01-10 01:46:59 -0800

    Added Ex2.7

---
+++ Chapter2/Ex2.7.scm
@@ -0,0 +1,8 @@
+(define (make-interval a b) (cons a b))
+
+(define (lower-bound interval)
+    (min (car interval) (cdr interval)))
+
+(define (upper-bound interval)
+    (max (car interval) (cdr interval)))
+
