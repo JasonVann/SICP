d0bc3030bb4f5c8e5ac8e3ca5957bd147eb300f6
JasonVann <jasonvanet@gmail.com>
2016-01-10 15:30:30 -0800

    Added Ex2.22

---
+++ Chapter2/Ex2.22.scm
@@ -0,0 +1,8 @@
+(define (square-list items)
+  (define (iter things answer)
+    (if (null? things)
+        (reverse answer)
+        (iter (cdr things)
+              (cons (square (car things))
+                    answer))))
+  (iter items nil))
