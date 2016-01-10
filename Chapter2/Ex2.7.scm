71538d49c067b1ace00caffbe9d6fed2571b28ea
JasonVann <jasonvanet@gmail.com>
2016-01-10 01:48:00 -0800

    Assuming that the lower bound may not necessarily on the left side

--- Chapter2/Ex2.7.scm
+++ Chapter2/Ex2.7.scm
@@ -1,5 +1,6 @@
 (define (make-interval a b) (cons a b))
 
+; We assum that the lower-bound may not necessarily on the left
 (define (lower-bound interval)
     (min (car interval) (cdr interval)))
 
