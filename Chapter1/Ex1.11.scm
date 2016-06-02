bfc291d8f6c4617f1f13a2ae932e580481605484
JasonVann <jasonvanet@gmail.com>
2016-06-01 20:38:33 -0700

    Added another iterative version

--- Chapter1/Ex1.11.scm
+++ Chapter1/Ex1.11.scm
@@ -13,3 +13,15 @@
   (if (< n 3) 
        n 
        (f-iter 2 1 0 n)))
+
+; Another iterative version
+(define (f n)
+  (define (f-iter a b c i n)
+    (cond
+          ((< n 3) n)
+          ((= n i) a)
+         (else (f-iter (+  a (* 2 b) (* 3 c)) a b (+ i 1) n))
+        )
+    )
+  (f-iter 2 1 0 1 n)
+  )
