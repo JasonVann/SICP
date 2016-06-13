42c69d0f276ffd9cb75dd75c4fc8225e06819280
JasonVann <jasonvanet@gmail.com>
2016-06-12 17:22:23 -0700

    Added new solution with sum approach

--- Chapter1/Ex1.29.scm
+++ Chapter1/Ex1.29.scm
@@ -8,3 +8,24 @@
     (* (/ (- b a) n 3) (+ (f 0) (iter 1))) 
 )
 
+; Alternatively
+(define (sum term a next b)
+  (if (> a b)
+      0
+      (+ (term a)
+         (sum term (next a) next b))))
+
+(define (simpson f a b n)
+  (define h (/ (- b a) n))
+  (define (y k)
+    (f (+ a (* k h))))
+  (define (term k)
+    (cond ((= k 0) (y 0)) 
+          ((= k n) (y k))
+          ((even? k) (* 2 (y k)))
+          (else (* 4 (y k)))
+          ))
+  (define (next k) (+ k 1))
+   (* (/ h 3) (sum term 0 next n))
+  )
+
