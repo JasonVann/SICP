f1d9b4af92a75bd612ec86f0b57d4fb2fd54a88e
JasonVann <jasonvanet@gmail.com>
2016-07-03 23:47:15 -0700

    Fixed typo

--- Chapter2/Ex2.25.scm
+++ Chapter2/Ex2.25.scm
@@ -3,11 +3,11 @@
 ; 7
 (car (cdr (car (cdr (cdr l)))))
 ; or
-(cadadd l)
+(car (cdaddr l))
 
 (define l (list (list 7)))
 (car (car l))
 
-(define z (list 1 (list 2 (list 3 (list 4 (list 5 (list 6 7)))))))
+(define l (list 1 (list 2 (list 3 (list 4 (list 5 (list 6 7)))))))
 
 (cadadr (cadadr (cadadr l)))
