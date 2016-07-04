f81e3c478388e7db17905ce87b7dd5c4ab6e9a74
JasonVann <jasonvanet@gmail.com>
2016-07-04 00:07:36 -0700

    Added sol to Ex2.28

--- Chapter2/Ex2.28.scm
+++ Chapter2/Ex2.28.scm
@@ -1,2 +1,11 @@
-(define fringe l)
-    (
+(define (fringe lst)
+  (if (null? lst) nil
+      (if (not (pair? lst)) (list lst)
+         (append (fringe (car lst)) (fringe (cdr lst)))
+         )))
+
+(define x (list (list 1 2 3 ) (list 4 5 6)))
+(fringe x)
+(fringe (list x x))
+
+
