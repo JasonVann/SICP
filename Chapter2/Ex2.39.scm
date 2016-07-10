445b1dc5b2145881ec24e22c7b06f49bd1f0c5a0
JasonVann <jasonvanet@gmail.com>
2016-07-09 22:31:06 -0700

    Added Ex2.39

---
+++ Chapter2/Ex2.39.scm
@@ -0,0 +1,14 @@
+(define (fold-left op initial sequence)
+  (define (iter result rest)
+    (if (null? rest)
+        result
+        (iter (op result (car rest))
+              (cdr rest))))
+  (iter initial sequence))
+
+(define (reverse sequence)
+  (fold-right (lambda (x y) (append y (list x))) nil sequence))
+
+(define (reverse sequence)
+  (fold-left (lambda (x y) (cons y x)) nil sequence))
+
