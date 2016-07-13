6414f2f6529d4e600d173ee591544bce0ce5518a
JasonVann <jasonvanet@gmail.com>
2016-07-13 00:54:12 -0700

    Added Ex3.7

---
+++ Chapter3/Ex3.7.scm
@@ -0,0 +1,41 @@
+(define (make-joint acc pw pw-new)
+  (lambda (pw2 mode)
+    (if (eq? pw2 pw-new)
+        (acc pw mode)
+        (print "wrong pw"))
+  ))
+
+(define (make-account balance password)
+  (define (withdraw amount)
+    (if (>= balance amount)
+        (begin (set! balance (- balance amount))
+               balance)
+        "Insufficient funds"))
+  (define (deposit amount)
+    (set! balance (+ balance amount))
+    balance)
+  (define (display-wrong-password-message useless-arg)              
+        (display "Incorrect password"))   
+  (define (dispatch pw m)
+    (if (eq? pw password)
+    (cond ((eq? m 'withdraw) withdraw)
+          ((eq? m 'deposit) deposit)
+          (else (error "Unknown request -- MAKE-ACCOUNT"
+                       m)))
+    ;(display "Incorrect password"))
+    display-wrong-password-message)
+    )
+  dispatch)
+
+(define acc (make-account 100 'secret-password))
+((acc 'secret-password 'withdraw) 140)
+((acc 'some-other-password 'deposit) 50)
+
+(print 'b)
+(display 'c)
+
+(define jack-acc (make-account 100 'jack-password))
+
+(define peter-acc (make-joint jack-acc 'jack-password 'peter-password))
+((peter-acc 'peter-password 'withdraw) 50)
+
